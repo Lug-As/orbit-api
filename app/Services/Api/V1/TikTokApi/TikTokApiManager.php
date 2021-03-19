e1e1ece0fcdc8bc4c79d4e4422e1e68480b20cb7
Artyom Sokol <skaliushartem@gmail.com>
2021-03-19 20:41:50 +0300

    Install TikTok API package

--- app/Services/Api/V1/TikTokApi/TikTokApiManager.php
+++ app/Services/Api/V1/TikTokApi/TikTokApiManager.php
@@ -7,6 +7,7 @@ namespace App\Services\Api\V1\TikTokApi;
 use App\Services\Api\V1\TikTokApi\DataObjects\AccountInfo;
 use Illuminate\Http\Client\Response as HttpResponse;
 use Illuminate\Support\Facades\Http;
+use Sovit\TikTok\Api as TikTokApi;
 
 class TikTokApiManager
 {
@@ -19,52 +20,14 @@ class TikTokApiManager
      */
     public function loadAccountInfo(string $name)
     {
-        $response = $this->requestInfo($name);
-        if ($response->ok()) {
-            $userInfo = $this->parseUserInfo($response);
-            if ($userInfo and isset($userInfo['stats']['followerCount']) and isset($userInfo['stats']['heartCount'])) {
-                $followers = $userInfo['stats']['followerCount'];
-                $likes = $userInfo['stats']['heartCount'];
-                return AccountInfo::make($followers, $likes);
-            }
-            return $this->argumentError();
+        $api = new TikTokApi();
+        $userInfo = $api->getUser(ltrim($name, '@'));
+        if ($userInfo and isset($userInfo->stats->followerCount) and isset($userInfo->stats->heartCount)) {
+            $followers = $userInfo->stats->followerCount;
+            $likes = $userInfo->stats->heartCount;
+            return AccountInfo::make($followers, $likes);
         }
-        return $this->apiError();
-    }
-
-    protected function requestInfo($name)
-    {
-        return Http::withHeaders([
-            'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 YaBrowser/20.9.0.928 Yowser/2.5 Safari/537.36',
-        ])
-            ->get("https://www.tiktok.com/{$name}?");
-    }
-
-    protected function parseUserInfo(HttpResponse $response)
-    {
-        $body = $response->body();
-        $props = $this->getStringBetween($body, self::START_SEPARATOR, self::END_SEPARATOR);
-        $json = json_decode($props, true);
-        if ($json and isset($json['props']['pageProps']['userInfo'])) {
-            return $json['props']['pageProps']['userInfo'];
-        }
-        return null;
-    }
-
-    protected function getStringBetween($string, $start, $end)
-    {
-        $string = ' ' . $string;
-        $ini = strpos($string, $start);
-        if ($ini == 0) return '';
-        $ini += strlen($start);
-        $len = strpos($string, $end, $ini) - $ini;
-        return substr($string, $ini, $len);
-    }
-
-    protected function apiError($msg = '')
-    {
-        //
-        return null;
+        return $this->argumentError();
     }
 
     protected function argumentError($msg = '')
